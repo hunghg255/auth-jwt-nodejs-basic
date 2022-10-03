51099ff4eb8f94a66a921d4bdd791204979158ee
hunghg255 <hunghg.agiletech@gmail.com>
2022-10-03 23:45:04 +0700

    update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const options = {
 };
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
