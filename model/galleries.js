b87725c498a35787184aff20b567edeeff84c1f8
hunghg255 <hunghg.agiletech@gmail.com>
2022-10-03 22:57:58 +0700

    update vercel

--- model/galleries.js
+++ model/galleries.js
@@ -3,22 +3,27 @@ const { faker } = require('@faker-js/faker');
 const galleries = [
   {
     id: faker.datatype.uuid(),
+    desctiption: faker.lorem.paragraph(),
     imageUrl: 'https://source.unsplash.com/collection/1758353/800x350/?sig=1',
   },
   {
     id: faker.datatype.uuid(),
+    desctiption: faker.lorem.paragraph(),
     imageUrl: 'https://source.unsplash.com/collection/1758353/800x350/?sig=2',
   },
   {
     id: faker.datatype.uuid(),
+    desctiption: faker.lorem.paragraph(),
     imageUrl: 'https://source.unsplash.com/collection/1758353/800x350/?sig=3',
   },
   {
     id: faker.datatype.uuid(),
+    desctiption: faker.lorem.paragraph(),
     imageUrl: 'https://source.unsplash.com/collection/1758353/800x350/?sig=4',
   },
   {
     id: faker.datatype.uuid(),
+    desctiption: faker.lorem.paragraph(),
     imageUrl: 'https://source.unsplash.com/collection/1758353/800x350/?sig=5',
   },
 ];
